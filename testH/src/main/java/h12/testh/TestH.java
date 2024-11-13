f563c9e95e0fec2652e292e4d1b4f9bed016fc89
Heinrich Drendorf <39944073+nachsommer@users.noreply.github.com>
2024-11-13 10:17:41 +0100

    initiating the TestH project.

---
+++ testH/src/main/java/h12/testh/TestH.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package h12.testh;
+
+/**
+ *
+ * @author mkr
+ */
+public class TestH {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
